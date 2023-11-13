fda18eb314c71a04de66c5c2c955a9b8035b30bd
Shogo Goto <gotoadmn0605@gmail.com>
2023-11-13 20:45:20 +0900

    check CI

---
+++ knowde/test_x.py
@@ -0,0 +1,11 @@
+"""check test for ci."""
+
+
+def test_x() -> None:
+    """Test x."""
+    print("aaaaaaaaaaaaa")  # noqa: T201
+
+
+def test_y() -> None:
+    """Test y."""
+    print("aaaaaaaaaaaaa")  # noqa: T201
