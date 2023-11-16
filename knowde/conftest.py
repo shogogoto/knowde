1d0d470a9ba7102f373ab0085a83f47617b8a377
Shogo Goto <gotoadmn0605@gmail.com>
2023-11-16 19:13:12 +0900

    suppress pytest warning

--- knowde/conftest.py
+++ knowde/conftest.py
@@ -7,13 +7,19 @@ from neomodel import clear_neo4j_database, config, db
 
 
 def pytest_configure() -> None:
-    """Run before tests."""
+    """Pytest hook."""
     config.DATABASE_URL = os.environ["NEO4J_BOLT_URL"]
     config.AUTO_INSTALL_LABELS = True
 
 
+def pytest_runtest_teardown() -> None:
+    """Pytest hook."""
+    if db.driver is not None:
+        db.close_connection()
+
+
 def pytest_collection_finish() -> None:
-    """Run after tests."""
+    """Pytest hook."""
     if db.driver is not None:
         clear_neo4j_database(db)
         db.close_connection()
