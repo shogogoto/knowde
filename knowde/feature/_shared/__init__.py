92d93c9e4ae56e6331161237cd996f404e8e62ec
Shogo Goto <gotoadmn0605@gmail.com>
2023-11-19 12:55:00 +0900

    rename neo4j url environment variable

--- knowde/feature/_shared/__init__.py
+++ knowde/feature/_shared/__init__.py
@@ -4,4 +4,7 @@ import os
 
 from neomodel import config
 
-config.DATABASE_URL = os.environ["NEO4J_BOLT_URL"]
+from .endpoint import Endpoint  # noqa: F401
+from .timeutil import TZ, jst_now  # noqa: F401
+
+config.DATABASE_URL = os.environ["NEO4J_URL"]
