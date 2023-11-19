d5d87035a891725277e15d9e1493fc8ec7546616
Shogo Goto <gotoadmn0605@gmail.com>
2023-11-19 17:36:02 +0900

    add typer based cli

--- knowde/feature/concept/cli.py
+++ knowde/feature/concept/cli.py
@@ -1,12 +1,14 @@
 """concept cli."""
+from __future__ import annotations
 
-from typing import Annotated
-from uuid import UUID
+from typing import Annotated, Optional
+from uuid import UUID  # noqa: TCH003
 
 import typer
 
 from knowde.feature._shared.endpoint import Endpoint
-from knowde.feature.concept.domain import ConceptProp
+from knowde.feature.concept.domain import ConceptChangeProp, ConceptProp
+from knowde.feature.concept.repo.api import req_list_concepts
 
 concept_cli = typer.Typer()
 
@@ -14,16 +16,35 @@ concept_cli = typer.Typer()
 @concept_cli.command("ls")
 def _list() -> None:
     """List concepts."""
-    typer.echo(Endpoint.Concept.get().json())
+    ls = req_list_concepts()
+    typer.echo(ls)
 
 
-@concept_cli.command()
-def add(c: ConceptProp) -> None:
+# createは打数が多いからやめた
+@concept_cli.command("add")
+def add(
+    name: Annotated[str, typer.Argument()],
+    explain: Annotated[Optional[str], typer.Argument()],
+) -> None:
     """Create concept."""
+    c = ConceptProp(name=name, explain=explain)
     Endpoint.Concept.post(json=c.model_dump())
 
 
 @concept_cli.command("rm")
-def remove(uid: Annotated[UUID, typer.Argument()]) -> None:
+def remove(
+    uid: Annotated[UUID, typer.Argument()],
+) -> None:
     """Remove concept."""
-    Endpoint.Concept.Delete(uid.hex)
+    Endpoint.Concept.delete(uid.hex)
+
+
+@concept_cli.command("ch")
+def change(
+    uid: UUID,
+    name: Annotated[Optional[str], typer.Option("-n")] = None,
+    explain: Annotated[Optional[str], typer.Option("-e")] = None,
+) -> None:
+    """Change concept properties."""
+    prop = ConceptChangeProp(name=name, explain=explain)
+    Endpoint.Concept.put(uid.hex, json=prop.model_dump())
