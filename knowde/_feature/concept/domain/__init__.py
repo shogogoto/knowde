150d1c8fbbaf05295afcf5e2b429bc8954a59a37
Shogo Goto <gotoadmn0605@gmail.com>
2023-11-25 14:43:50 +0900

    fix lint

--- knowde/_feature/concept/domain/__init__.py
+++ knowde/_feature/concept/domain/__init__.py
@@ -1,3 +1,3 @@
 """concept domain init."""
-from .domain import Concept, ChangeProp, ConceptProp  # noqa: F401
+from .domain import ChangeProp, Concept, ConceptProp  # noqa: F401
 from .rel import AdjacentConcept, ConnectedConcept, ConnectionType  # noqa: F401
