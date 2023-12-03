8e86f8e0e822d4edebb7cf125164d655bc6aaa79
Shogo Goto <gotoadmn0605@gmail.com>
2023-12-03 22:23:01 +0900

    add create, delete api for src dest

--- knowde/_feature/concept/api/__init__.py
+++ knowde/_feature/concept/api/__init__.py
@@ -1,2 +1,3 @@
 from .adjacent import concept_adj_router  # noqa: F401
 from .api import concept_router  # noqa: F401
+from .src_dest import concept_dest_router, concept_src_router  # noqa: F401
--- knowde/api.py
+++ knowde/api.py
@@ -3,8 +3,15 @@ from __future__ import annotations
 
 from fastapi import FastAPI
 
-from ._feature import concept_adj_router, concept_router
+from ._feature import (
+    concept_adj_router,
+    concept_dest_router,
+    concept_router,
+    concept_src_router,
+)
 
 api = FastAPI()
 api.include_router(concept_router)
 api.include_router(concept_adj_router)
+api.include_router(concept_src_router)
+api.include_router(concept_dest_router)
