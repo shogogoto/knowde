98888212d35cb5224c1caa173c612b73c43c3ea7
Shogo Goto <gotoadmn0605@gmail.com>
2024-01-04 20:35:34 +0900

    ApiParamのfiledsを引数にする関数を生成

--- knowde/_feature/_shared/api/param.py
+++ knowde/_feature/_shared/api/param.py
@@ -1,13 +1,45 @@
+from __future__ import annotations
+
+from inspect import Parameter, Signature, signature
+from typing import TYPE_CHECKING, Callable, Generic, Self, TypeVar
+
+from makefun import create_function
 from pydantic import BaseModel
 
-from knowde._feature._shared.repo.base import LBase
+from knowde._feature._shared.domain import DomainModel
+
+if TYPE_CHECKING:
+    from knowde._feature._shared.repo.base import LBase
+
+T = TypeVar("T", bound=DomainModel)
 
 
-class ApiParam(BaseModel, frozen=True):
+class ApiParam(BaseModel, Generic[T], frozen=True):
     def set_attr(self, lb: LBase) -> None:
         for k, v in self.model_fields:
             setattr(lb, k, v)
 
-    # def make_req(self) -> None:
-    #     for k, v in self.model_fields.items():
-    #         create_function()
+    @classmethod
+    def makefunc(
+        cls,
+        f: Callable[[Self], T],
+        func_name: str | None = None,
+        doc: str | None = None,
+    ) -> Callable:
+        params = []
+        for k, v in cls.model_fields.items():
+            kind = Parameter.KEYWORD_ONLY
+            p = Parameter(k, kind=kind, annotation=v.annotation)
+            params.append(p)
+        r = signature(f).return_annotation
+        sig = Signature(params, return_annotation=r)
+
+        def impl(**kwargs) -> T:  # noqa: ANN003
+            return f(cls(**kwargs))
+
+        return create_function(
+            sig,
+            impl,
+            func_name=func_name,
+            doc=doc,
+        )
--- knowde/_feature/_shared/api/test_param.py
+++ knowde/_feature/_shared/api/test_param.py
@@ -1,11 +1,21 @@
+from knowde._feature._shared.domain import DomainModel
+
 from .param import ApiParam
 
 
-class OnePatam(ApiParam, frozen=True):
+class OneParam(ApiParam, frozen=True):
     p1: str
     p2: int
 
 
-# def test_makefunc() -> None:
-#     op = OnePatam(p1="s", p2=0)
-#     print(op.makefunc())
+class Model(DomainModel, frozen=True):
+    p3: str
+    p4: int
+
+
+def test_signature() -> None:
+    def x(p: OneParam) -> Model:
+        return Model(p3=p.p1, p4=p.p2)
+
+    f = OneParam.makefunc(x)
+    assert f(p1="p1", p2=0) == Model(p3="p1", p4=0)
