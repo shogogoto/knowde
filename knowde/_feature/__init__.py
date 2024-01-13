fd139281216f416f2cadbf2d3ca679386b912b4d
Shogo Goto <gotoadmn0605@gmail.com>
2024-01-13 19:04:16 +0900

    add sentence api

--- knowde/_feature/__init__.py
+++ knowde/_feature/__init__.py
@@ -1,3 +1,4 @@
 """independent features."""
 from .concept import *  # noqa: F403
 from .reference import *  # noqa: F403
+from .sentence import *  # noqa: F403
---
+++ knowde/_feature/sentence/__init__.py
@@ -0,0 +1,1 @@
+from .api import s_router  # noqa: F401
