e0fc43a1d023abd77eda8a0d0da5c8ec4f19406d
Shogo Goto <gotoadmn0605@gmail.com>
2024-01-17 18:36:56 +0900

    click decoratorsを同一ディレクトリへまとめた

--- knowde/_feature/_shared/cli/__init__.py
+++ knowde/_feature/_shared/cli/__init__.py
@@ -1,3 +1,2 @@
+from .click_decorators import *  # noqa: F403
 from .create_group import create_group  # noqa: F401
-from .each_args import each_args  # noqa: F401
-from .view import *  # noqa: F403
