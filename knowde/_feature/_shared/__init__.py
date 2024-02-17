11c3e7102799dbb9f966165b8465ebf8419a542d
Shogo Goto <gotoadmn0605@gmail.com>
2024-02-17 16:39:32 +0900

    fix export

--- knowde/_feature/_shared/__init__.py
+++ knowde/_feature/_shared/__init__.py
@@ -9,7 +9,6 @@ from .cli import *  # noqa: F403
 from .domain import DomainModel  # noqa: F401
 from .errors import *  # noqa: F403
 from .repo import *  # noqa: F403
-from .timeutil import TZ, jst_now  # noqa: F401
 from .types import Graph, NeoModel  # noqa: F401
 
 config.DATABASE_URL = os.environ["NEO4J_URL"]
