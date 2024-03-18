0541f37573ae45674606682cd96c1e7fc081483c
Shogo Goto <gotoadmn0605@gmail.com>
2024-03-18 16:02:20 +0900

    fix networkx.DiGraph type for pydantic

--- knowde/_feature/_shared/__init__.py
+++ knowde/_feature/_shared/__init__.py
@@ -9,6 +9,6 @@ from .cli import *  # noqa: F403
 from .domain import DomainModel  # noqa: F401
 from .errors import *  # noqa: F403
 from .repo import *  # noqa: F403
-from .types import Graph, NeoModel  # noqa: F401
+from .types import NeoModel, NXGraph  # noqa: F401
 
 config.DATABASE_URL = os.environ["NEO4J_URL"]
