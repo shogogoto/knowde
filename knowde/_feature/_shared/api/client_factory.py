8c0e3bc932fc3dd8ffe20d8667c2b139fcecdcd7
Shogo Goto <gotoadmn0605@gmail.com>
2024-03-23 12:17:33 +0900

    remove specific client

--- knowde/_feature/_shared/api/client_factory.py
+++ knowde/_feature/_shared/api/client_factory.py
@@ -1,47 +1,24 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Callable, Self
-from uuid import UUID
+from typing import TYPE_CHECKING, Callable, Self, TypeVar
 
-from fastapi import APIRouter  # noqa:TCH002
+from compose import compose
 from pydantic import BaseModel, Field
-from pydantic_partial.partial import create_partial_model
 
-from knowde._feature._shared.api.client import (
-    add_client,
-    change_client,
-    complete_client,
-    list_client,
-    remove_client,
-)
 from knowde._feature._shared.api.client_param import (
     BodyParam,
     ComplexQueryParam,
     PathParam,
     QueryParam,
 )
-from knowde._feature._shared.api.endpoint_funcs import EndpointFuncs
-from knowde._feature._shared.api.types import (
-    Add,
-    BasicClients,
-    Change,
-    CheckResponse,
-    Complete,
-    ListClient,
-    Remove,
-    ToRequest,
-)
-from knowde._feature._shared.repo.util import LabelUtil  # noqa: TCH001
-
-from .generate_req import (
-    StatusCodeGrant,
-    inject_signature,
-)
+from knowde._feature._shared.domain import APIReturn
 
 if TYPE_CHECKING:
     import requests
 
-    from knowde._feature._shared.domain import DomainModel
+    from knowde._feature._shared.api.types import (
+        ToRequest,
+    )
 
 
 class RequestPartial(BaseModel):
@@ -83,126 +60,18 @@ class RequestPartial(BaseModel):
         return self
 
 
-def create_add_client(
-    router: APIRouter,
-    f: Callable,
-    t_in: type[BaseModel],
-    t_out: type[DomainModel],
-    check_response: CheckResponse | None = None,
-) -> Add:
-    reqs = StatusCodeGrant(router=router)
-    req_add = reqs.to_post(
-        inject_signature(f, [t_in], t_out),
-    )
-    return add_client(req_add, t_in, t_out, check_response)
+T = TypeVar("T", bound=APIReturn)
 
 
-def create_change_client(
-    router: APIRouter,
-    f: Callable,
-    t_in: type[BaseModel],
-    t_out: type[DomainModel],
-    complete_client: Complete,
-) -> Change:
-    reqs = StatusCodeGrant(router=router)
-    OPT = create_partial_model(t_in)  # noqa: N806
-    req_ch = reqs.to_put(
-        inject_signature(f, [UUID, OPT], t_out),
-    )
-    return change_client(req_ch, OPT, t_out, complete_client)
+def to_client(
+    req: Callable[..., requests.Response],
+    t: type[T],
+) -> Callable[..., T]:
+    return compose(t.of, req)
 
 
-def create_complete_client(
-    router: APIRouter,
-    f: Complete,
-    t_out: type[DomainModel],
-) -> Complete:
-    reqs = StatusCodeGrant(router=router)
-    req_complete = reqs.to_get(
-        inject_signature(f, [str], t_out),
-        "/completion",
-    )
-    return complete_client(req_complete, t_out)
-
-
-def create_remove_client(
-    router: APIRouter,
-    f: Remove,
-) -> Remove:
-    reqs = StatusCodeGrant(router=router)
-    req_rm = reqs.to_delete(inject_signature(f, [UUID]))
-    return remove_client(req_rm)
-
-
-def create_list_client(
-    router: APIRouter,
-    f: Callable,
-    t_out: type[DomainModel],
-    t_in: type[BaseModel] | None = None,
-    check_response: CheckResponse | None = None,
-) -> ListClient:
-    reqs = StatusCodeGrant(router=router)
-    if t_in is None:
-        f_di = inject_signature(f, [], list[t_out])
-    else:
-        f_di = inject_signature(f, [t_in], list[t_out])
-    req_ls = reqs.to_get(f_di)
-    return list_client(req_ls, t_out, t_in, check_response)
-
-
-def create_basic_clients(
-    util: LabelUtil,
-    router: APIRouter,
-) -> BasicClients:
-    """labelに対応したCRUD APIの基本的な定義."""
-    epfs = EndpointFuncs(util=util)
-    return BasicClients(
-        create_remove_client(router, epfs.rm),
-        create_complete_client(router, epfs.complete, util.model),
-        create_list_client(router, epfs.ls, util.model),
-    )
-
-
-class APIClientFactory(
-    BaseModel,
-    frozen=True,
-    arbitrary_types_allowed=True,
-):
-    util: LabelUtil
-    router: APIRouter
-
-    @property
-    def t_out(self) -> type[DomainModel]:
-        return self.util.model
-
-    def create_basics(self) -> BasicClients:
-        return create_basic_clients(
-            self.util,
-            self.router,
-        )
-
-    def create_add(
-        self,
-        t_in: type[BaseModel],
-    ) -> Add:
-        epfs = EndpointFuncs(util=self.util)
-        return create_add_client(
-            self.router,
-            epfs.add_factory(t_in),
-            t_in,
-            self.util.model,
-        )
-
-    def create_change(
-        self,
-        t_in: type[BaseModel],
-    ) -> Change:
-        epfs = EndpointFuncs(util=self.util)
-        f = epfs.ch_factory(t_in)
-        return create_change_client(
-            self.router,
-            f,
-            t_in,
-            self.util.model,
-            self.create_basics().complete,
-        )
+def to_client_return_list(
+    req: Callable[..., requests.Response],
+    t: type[T],
+) -> Callable[..., list[T]]:
+    return compose(t.ofs, req)
