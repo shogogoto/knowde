acd20c6f5b9b9a603b035bf74ee075f19abf177c
Shogo Goto <gotoadmn0605@gmail.com>
2024-03-23 11:30:54 +0900

    delete concept package

--- knowde/_feature/concept/api/util.py
+++
@@ -1,6 +0,0 @@
-from knowde._feature._shared import Endpoint
-
-_EP = Endpoint.Concept
-
-PREFIX = f"/{_EP.value}"
-TAG = f"{_EP.single_form}"
--- knowde/_feature/concept/domain/__init__.py
+++
@@ -1,9 +0,0 @@
-"""concept domain init."""
-from .domain import (  # noqa: F401
-    AdjacentIdsProp,
-    ChangeProp,
-    Concept,
-    ConceptProp,
-    SaveProp,
-)
-from .rel import AdjacentConcept, ConnectedConcept, ConnectionType  # noqa: F401
