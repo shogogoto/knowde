c25282e1c96dfd55193263a3906efb5f1e6a1833
Shogo Goto <gotoadmn0605@gmail.com>
2024-04-04 18:18:23 +0900

    add type date, datetime for to_clickparam

--- knowde/_feature/_shared/cli/to_click.py
+++ knowde/_feature/_shared/cli/to_click.py
@@ -1,27 +1,59 @@
 from __future__ import annotations
 
-from typing import Callable, TypeAlias
+from datetime import date, datetime
+from typing import Any, Callable, Optional, TypeAlias
 from uuid import UUID
 
 import click
-from click import ParamType
+from click import Context, Parameter, ParamType
 from click.decorators import FC
 from pydantic import BaseModel
 
+from knowde._feature._shared.domain import TZ
+
+
+class DateType(ParamType):
+    name = "date"
+    _format = "%Y-%m-%d"
+
+    def _try_to_convert(self, value: Any) -> Optional[date]:  # noqa: ANN401
+        try:
+            return datetime.strptime(value, self._format).astimezone(TZ).date()
+        except ValueError:
+            return None
+
+    def convert(
+        self,
+        value: Any,  # noqa: ANN401
+        param: Optional[Parameter],
+        ctx: Optional[Context],
+    ) -> date:
+        if isinstance(value, date):
+            return value
+        c = self._try_to_convert(value)
+        if c is not None:
+            return c
+        self.fail(  # noqa: RET503
+            f"{value} does not match the format {self._format}.",
+            param,
+            ctx,
+        )
+
 
 def to_clicktype(t: type) -> ParamType:
-    if t == str:
-        return click.STRING
-    if t == float:
-        return click.FLOAT
-    if t == UUID:
-        return click.UUID
-    if t == int:
-        return click.INT
-    if t == bool:
-        return click.BOOL
-    msg = f"{t} is not compatible type"
-    raise TypeError(msg)
+    t_map: dict[type, ParamType] = {
+        str: click.STRING,
+        float: click.FLOAT,
+        UUID: click.UUID,
+        bool: click.BOOL,
+        int: click.INT,
+        date: DateType(),
+        datetime: click.types.DateTime(),
+    }
+    if t not in t_map:
+        msg = f"{t} is not compatible type"
+        raise TypeError(msg)
+    return t_map[t]
 
 
 ClickParam: TypeAlias = Callable[[FC], FC]
