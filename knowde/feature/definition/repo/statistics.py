a0ca7e8301804c52b5906716a6184b836785be89
Shogo Goto <gotoadmn0605@gmail.com>
2024-06-23 18:14:08 +0900

    3layer stats

--- knowde/feature/definition/repo/statistics.py
+++ knowde/feature/definition/repo/statistics.py
@@ -22,25 +22,15 @@ def find_statistics(def_uid: UUID) -> DepStatistics:
         OPTIONAL MATCH root = (s)<-[:{mn}|{dn}]-+(rt:Sentence)
             WHERE NOT (rt)<-[:{mn}]-(:Term)
         RETURN
-            leaf,
-            root,
             count(DISTINCT s_src) as n_src,
             count(DISTINCT s_dest) as n_dest,
             // s1<-[:def]<-t1<-[:mark]<-s2 というように2つのrelがあるので割る2
-            length(leaf) / 2 as max_leaf_dist,
-            length(root) / 2 as max_root_dist
+            max(length(leaf)) / 2 as max_leaf_dist,
+            max(length(root)) / 2 as max_root_dist
         """,
         params={"uid": def_uid.hex},
     )
 
-    # print("################################## res")
-    # print("################################## res")
-    # for row in res.get("leaf"):
-    #     print(row)
-    # print("################################## res")
-
-    # for row in res.get("root"):
-    #     print(row)
     return DepStatistics(
         n_src=res.get("n_src")[0],
         n_dest=res.get("n_dest")[0],
