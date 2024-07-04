418057a330d7d3ab353c8ca2cb2f6f995555fc66
Shogo Goto <gotoadmn0605@gmail.com>
2024-07-04 16:17:21 +0900

    deductのpremises順序がランダムになるっぽい？

--- knowde/feature/proposition/repo/test_deduction.py
+++ knowde/feature/proposition/repo/test_deduction.py
@@ -1,10 +1,21 @@
 """論証のテスト."""
+from __future__ import annotations
 
+from typing import TYPE_CHECKING
 
 from pytest_unordered import unordered
 
 from knowde.feature.proposition.repo.deduction import deduct, list_deductions
 from knowde.feature.proposition.repo.proposition import add_proposition
+
+if TYPE_CHECKING:
+    from uuid import UUID
+
+    from knowde.feature.proposition.domain import Proposition
+
+
+def _uids(*ps: Proposition) -> list[UUID]:
+    return [p.valid_uid for p in ps]
 
 
 def test_simple_add() -> None:
@@ -13,13 +24,12 @@ def test_simple_add() -> None:
     p2 = add_proposition("p2")
     p3 = add_proposition("p3")
     p4 = add_proposition("p4")
+    p5 = add_proposition("p5")
+    p6 = add_proposition("p6")
+    p7 = add_proposition("p7")
     c = add_proposition("conclusion")
     txt = "これこれよりQED"
-    d = deduct(
-        txt,
-        [p1.valid_uid, p2.valid_uid, p3.valid_uid, p4.valid_uid],
-        c.valid_uid,
-    )
+    d = deduct(txt, _uids(p1, p2, p3, p4, p5, p6, p7), c.valid_uid)
     assert list_deductions() == [d]
 
 
@@ -29,6 +39,6 @@ def test_2chain() -> None:
     p2 = add_proposition("p2")
     p3 = add_proposition("p3")
     p4 = add_proposition("p4")
-    d1 = deduct("xxx", [p1.valid_uid, p2.valid_uid], p3.valid_uid)
+    d1 = deduct("xxx", _uids(p1, p2), p3.valid_uid)
     d2 = deduct("yyy", [p3.valid_uid], p4.valid_uid)
     assert list_deductions() == unordered([d1, d2])
