d9252ed4093cf16ecebcfc68e7388ca3f08c89a4
Shogo Goto <gotoadmn0605@gmail.com>
2024-10-05 09:11:14 +0900

    用語と用語空間

--- knowde/feature/parser/domain/term/domain.py
+++ knowde/feature/parser/domain/term/domain.py
@@ -1,14 +1,40 @@
 """用語モデル."""
 from __future__ import annotations
 
-from pydantic import BaseModel, Field
+from collections import Counter
+from typing import Self
+
+from pydantic import BaseModel, Field, field_validator
 
 
-class TermGroup(BaseModel, frozen=True):
-    """特定の言明に割り当てられた用語のグループ."""
+class Term(BaseModel, frozen=True):
+    """用語 名前のあつまり."""
 
-    rep: str = Field(title="代表名")  # representativeが長いので略
-    aliases: list[str] = Field(default_factory=list)
+    names: list[str] = Field(default_factory=list, min_length=1)
+
+    @field_validator("names")
+    @classmethod
+    def _validate(cls, vs: list[str]) -> list[str]:
+        """Validate duplication."""
+        for k, v in Counter(vs).items():
+            if v > 1:
+                msg = f"{k}が複数回{v}宣言されています"
+                raise ValueError(msg)
+        return vs
+
+    @property
+    def rep(self) -> str:
+        """代表名."""
+        return self.names[0]
+
+    @property
+    def aliases(self) -> list[str]:
+        """別名リスト."""
+        return self.names[1:]
+
+    @classmethod
+    def create(cls, *vs: str) -> Self:  # noqa: D102
+        return cls(names=list(vs))
 
     def __str__(self) -> str:
         """Display for user."""
@@ -16,3 +42,92 @@ class TermGroup(BaseModel, frozen=True):
         if len(al) > 0:
             al = f"({al})"
         return f"{self.rep}{al}"
+
+    def has(self, *names: str) -> bool:
+        """同じ名前を持つ."""
+        s1 = set(self.names)
+        s2 = set(names)
+        common = s1.intersection(s2)
+        return len(common) > 0
+
+    def has_only_name(self) -> bool:
+        """単名を持つ."""
+        return len(self.names) == 1
+
+    def allows_merge(self, other: Term) -> bool:
+        """合併を許すか否か."""
+        # 共通なし -> 合併しない
+        # 共通あり
+        #   完全一致 -> False
+        #     -> True
+        if not self.has(*other.names):
+            return False
+        # if self == other:
+        #     return False
+        # return True
+        has_only = self.has_only_name() or other.has_only_name()
+        return not has_only
+
+    def merge(self, other: Term) -> Term:
+        """名前を併せた用語へ."""
+        if not self.allows_merge(other):
+            raise TermMergeError(self, other)
+        s1 = set(self.names)
+        s2 = set(other.names)
+        diff = s2.difference(s1)
+        return Term(names=self.names + list(diff))
+
+    @property
+    def d(self) -> dict[str, Self]:
+        """HashMapによる検索に使えるかな."""
+        return {k: self for k in self.names}
+
+
+class TermMergeError(Exception):
+    """用語の合併を許さないのに."""
+
+
+class TermConflictError(Exception):
+    """用語の衝突."""
+
+
+class TermSpace(BaseModel):
+    """用語空間.
+
+    名前一覧を表現
+    名前 -> 用語
+
+    """
+
+    terms: list[Term] = Field(default_factory=list)
+    origins: list[Term] = Field(default_factory=list)
+
+    def __getitem__(self, i: int) -> Term:  # noqa: D105
+        return self.terms[i]
+
+    def __len__(self) -> int:  # noqa: D105
+        return len(self.terms)
+
+    def add(self, t: Term) -> None:
+        """名前."""
+        if t in self.origins:
+            msg = "用語定義が重複しています"
+            raise TermConflictError(msg, t)
+        c = self.common(t)
+        if c is None:
+            self.terms.append(t)
+        else:
+            self.terms.remove(c)
+            self.terms.append(c.merge(t))
+        self.origins.append(t)
+
+    def common(self, t: Term) -> Term | None:
+        """共通する名を持つ用語があれば返す."""
+        ls = [v for v in self.terms if t.has(*v.names)]
+        match len(ls):
+            case 0:
+                return None
+            case 1:
+                return ls[0]
+            case _:
+                raise TermConflictError
--- knowde/feature/parser/domain/term/test_domain.py
+++ knowde/feature/parser/domain/term/test_domain.py
@@ -1,10 +1,95 @@
-"""terst term domain."""
-from knowde.feature.parser.domain.term.domain import TermGroup
+"""test term domain."""
 
 
-def test_term_group() -> None:
-    """String test."""
-    tg1 = TermGroup(rep="X", aliases=["x1", "x2"])
-    tg2 = TermGroup(rep="Y")
-    assert str(tg1) == "X(x1, x2)"
-    assert str(tg2) == "Y"
+import pytest
+
+from knowde.feature.parser.domain.term.domain import (
+    Term,
+    TermConflictError,
+    TermMergeError,
+    TermSpace,
+)
+
+"""
+Termは名前の集合に与える識別子
+A=B=C:
+は {A, B, C} -> term -> 言明
+という中間層
+
+重複した名の宣言は禁止
+ただし、実はこの名も宣言済みの名と同じ概念であることが読書のあとになって発覚する場合がある
+
+一覧のスコープ:
+    ソースごと
+    見出しごと
+
+表示形式:
+    言明付き
+    言明の依存関係情報
+"""
+
+
+def test_term_str() -> None:
+    """String."""
+    t1 = Term.create("X", "x1", "x2")
+    t2 = Term.create("Y", "y1")
+    t3 = Term.create("Z")
+    assert str(t1) == "X(x1, x2)"
+    assert str(t2) == "Y(y1)"
+    assert str(t3) == "Z"
+
+
+def test_term_has_common() -> None:
+    """同じ名前を持つ用語."""
+    t1 = Term.create("X", "x1")
+    t2 = Term.create("X")
+    assert t1.has(*t2.names)
+    assert t2.has(*t1.names)
+
+    t3 = Term.create("Y")
+    assert not t1.has(*t3.names)
+    assert not t2.has(*t3.names)
+    assert not t3.has(*t1.names)
+    assert not t3.has(*t2.names)
+
+
+def test_term_allows_merge() -> None:
+    """用語の結合を許す場合."""
+    # 共通あり
+    t1 = Term.create("X", "x1")
+    t2 = Term.create("X", "x2")
+    assert t1.allows_merge(t2)
+
+    ## 共通あっても単一名はダメ
+    t3 = Term.create("X")
+    assert not t1.allows_merge(t3)
+    with pytest.raises(TermMergeError):
+        t1.merge(t3)
+    # 共通なし
+    t4 = Term.create("Y")
+    assert not t1.allows_merge(t4)
+
+
+def test_termspace_add() -> None:
+    """用語空間に用語を追加."""
+    s = TermSpace()
+    # 共通ありで合併
+    t1 = Term.create("X", "x1")
+    t2 = Term.create("X", "x2")
+    s.add(t1)
+    s.add(t2)
+    assert len(s) == 1
+    assert s[0] == Term.create("X", "x1", "x2")
+
+    # 重複の追加はエラー
+    with pytest.raises(TermConflictError):
+        s.add(t2)
+
+    # 共通なしの追加
+    t3 = Term.create("Y")
+    s.add(t3)
+    assert len(s) == 2  # noqa: PLR2004
+
+    # 共通あっても単一名はダメ
+    with pytest.raises(TermMergeError):
+        s.add(Term.create("X"))
