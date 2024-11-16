c3e89ce1ff068968c4b7af125c3893119a550759
Shogo Goto <gotoadmn0605@gmail.com>
2024-11-17 00:06:24 +0900

    fix lint

--- knowde/complex/system/domain/term/resolver.py
+++ knowde/complex/system/domain/term/resolver.py
@@ -1,9 +1,14 @@
+"""用語解決."""
 from __future__ import annotations
 
 from pydantic import BaseModel
 
 
 class TermResolver(BaseModel):
-    """temp."""
+    """用語解決器."""
 
     atomics: dict[str, str]
+
+
+# def resolve_term(term: str, d: dict) -> None:
+#     pass
