8afe9c632634a64835d7b98d8a6fd407dca05df7
Shogo Goto <gotoadmn0605@gmail.com>
2024-11-17 17:25:32 +0900

    ゴミ削除

--- knowde/feature/parser/domain/term/errors.py
+++
@@ -1,9 +0,0 @@
-"""term errors."""
-
-
-class TermMergeError(Exception):
-    """用語の合併を許さないのに."""
-
-
-class TermConflictError(Exception):
-    """用語の衝突."""
--- knowde/feature/parser/interface.py
+++ knowde/feature/parser/interface.py
@@ -4,22 +4,21 @@ from typing import IO
 import click
 
 from knowde.feature.parser.domain.parser.parser import transparse
-from knowde.feature.parser.domain.term.visitor import get_termspace
 
 
 @click.command("parse")
 @click.argument("stdin", type=click.File("r"), default="-")
 def parse_cmd(stdin: IO) -> None:
     """Stdin."""
-    tree = transparse(stdin.read())
-    s = get_termspace(tree)
-    click.echo(s.pretty())
+    _tree = transparse(stdin.read())
+    # s = get_termspace(tree)
+    # click.echo(s.pretty())
 
 
 @click.command("parse2")
 @click.argument("stdin", type=click.File("r"), default="-")
 def parse2_cmd2(stdin: IO) -> None:
     """Stdin."""
-    tree = transparse(stdin.read())
-    s = get_termspace(tree)
-    click.echo(s.pretty())
+    _tree = transparse(stdin.read())
+    # s = get_termspace(tree)
+    # click.echo(s.pretty())
