28272b18b9c47736eaa3b3f67c9104d2bcbf06e4
Shogo Goto <gotoadmn0605@gmail.com>
2024-11-20 15:32:20 +0900

    move timeline package

--- knowde/primitive/timeline/__init__.py
+++ knowde/primitive/timeline/__init__.py
@@ -1,7 +1,13 @@
 """時系列."""
+
+from typing import Final
+
 from pydantic import BaseModel
 
 from knowde.core.types import NXGraph
+
+# = Anno Domini, 主の年に, 西暦紀元後
+SOCIETY_TIMELINE: Final = "AD"
 
 
 class TimeSeries(BaseModel):
