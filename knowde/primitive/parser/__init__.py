f8cd2e5aae390c849230256c58c1fc83ee68b965
Shogo Goto <gotoadmn0605@gmail.com>
2024-12-15 20:57:35 +0900

    parse line

--- knowde/primitive/parser/__init__.py
+++ knowde/primitive/parser/__init__.py
@@ -27,11 +27,33 @@ structure_parser = Lark(
     postlex=SampleIndenter(),
 )
 
-line_parser = Lark(
-    (Path(__file__).parent / "grammer/line.lark").read_text(),
-    parser="lalr",
-    postlex=SampleIndenter(),
-)
+# line_parser = Lark(
+#     (Path(__file__).parent / "grammer/line.lark").read_text(),
+#     parser="lalr",
+# )
+
+ALIAS_SEP = "|"
+DEF_SEP = ":"
+NAME_SEP = ","
+
+
+# larkでのparseが思ったようにいかなかったからあきらめた
+#   aliasやname, sentenceで許容する文字列を思ったように設定できなかった
+def parse_line(line: str) -> tuple[str | None, list[str], str | None]:
+    """行を解析."""
+    txt = dedent(line).strip()
+    alias = None
+    names = []
+    define = txt
+    sentence = txt
+    if ALIAS_SEP in txt:
+        alias, define = txt.split(ALIAS_SEP)
+        alias = alias.strip()
+        sentence = define
+    if DEF_SEP in define:
+        names, sentence = define.split(DEF_SEP)
+        names = [n.strip() for n in names.split(NAME_SEP)]
+    return alias, names, sentence.strip() if sentence else None
 
 
 def parse2tree(text: str) -> Tree:
@@ -39,9 +61,7 @@ def parse2tree(text: str) -> Tree:
     txt = dedent(text)
     parser = structure_parser
     try:
-        return parser.parse(
-            txt,  # , on_error=handle_error,
-        )
+        return parser.parse(txt)  # , on_error=handle_error,
     except UnexpectedInput as e:
         exc_class = e.match_examples(
             parser.parse,
