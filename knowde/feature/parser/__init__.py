c84ffa5e642e6e65a714a85ddc81485208634eaa
Shogo Goto <gotoadmn0605@gmail.com>
2024-12-22 20:03:38 +0900

    bit change for parse command

--- knowde/feature/parser/__init__.py
+++ knowde/feature/parser/__init__.py
@@ -1,14 +1,10 @@
 # ruff: noqa
 """load-file."""
 
-from pprint import pp
 from typing import IO
 
 import click
 from lark import LarkError
-import networkx as nx
-
-from knowde.core.errors.domain import MultiHitError
 from knowde.core.nxutil import nxprint
 from knowde.core.nxutil.errors import MultiEdgesError
 from knowde.feature.parser.tree2net import parse2net
@@ -22,11 +18,11 @@ def parse_cmd(stdin: IO) -> None:
     txt = stdin.read()
     try:
         sn = parse2net(txt, True)
-        nxprint(sn.graph, True)
-        pr = nx.pagerank(sn.graph)
-        pr = {sn.get(k): v for k, v in pr.items()}
-        pp(pr)
-        pp(sorted(pr.items(), key=lambda i: i[1]))
+        nxprint(sn.graph)
+        # pr = nx.pagerank(sn.graph)
+        # pr = {sn.get(k): v for k, v in pr.items()}
+        # pp(pr)
+        # pp(sorted(pr.items(), key=lambda i: i[1]))
     except ParserError as e:
         print(e)  # noqa: T201
     except LarkError as e:
