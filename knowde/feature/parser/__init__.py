f60d280897062685c3fe54445b80da6d6e4eaa78
Shogo Goto <gotoadmn0605@gmail.com>
2024-12-26 13:58:49 +0900

    super error class

--- knowde/feature/parser/__init__.py
+++ knowde/feature/parser/__init__.py
@@ -5,10 +5,14 @@ from typing import IO
 import click
 from lark import LarkError
 
+from knowde.complex.__core__.sysnet.errors import (
+    InterpreterError,
+)
 from knowde.feature.parser.tree2net import parse2net
+from knowde.feature.parser.tree2net.ranking import get_ranking
 from knowde.primitive.__core__.nxutil.errors import MultiEdgesError
 from knowde.primitive.parser.errors import ParserError
-from knowde.primitive.term.errors import MarkUncontainedError
+from knowde.primitive.term.errors import TermError
 
 
 @click.command("parse")
@@ -18,15 +22,18 @@ def parse_cmd(stdin: IO) -> None:
     txt = stdin.read()
     try:
         _sn = parse2net(txt)
+        get_ranking(_sn)
         # pr = nx.pagerank(sn.graph)
         # pr = {sn.get(k): v for k, v in pr.items()}
         # pp(pr)
         # pp(sorted(pr.items(), key=lambda i: i[1]))
-    except ParserError as e:
-        print(e)  # noqa: T201
     except LarkError as e:
         print(e)  # noqa: T201
-    except MultiEdgesError as e:
+    except ParserError as e:
         print(e)  # noqa: T201
-    except MarkUncontainedError as e:
+    except TermError as e:
+        print(e)  # noqa: T201
+    except InterpreterError as e:
+        print(e)  # noqa: T201
+    except MultiEdgesError as e:
         print(e)  # noqa: T201
