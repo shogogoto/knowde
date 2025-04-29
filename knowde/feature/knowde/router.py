e41e87a735988d7a5474d023f9c31f3ce1edf0cd
Shogo Goto <gotoadmn0605@gmail.com>
2025-04-29 11:02:53 +0900

    search の orderが効かなかったから直した & デフォルト値変更

--- knowde/feature/knowde/router.py
+++ knowde/feature/knowde/router.py
@@ -2,7 +2,7 @@
 
 from functools import cache
 
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, Depends, Query
 from pydantic import BaseModel, Field
 
 from knowde.feature.knowde import KAdjacency
@@ -19,15 +19,45 @@ def knowde_router() -> APIRouter:
 class SearchParam(BaseModel):
     """検索パラメータ."""
 
-    q: str = ".*"
-    type: str = Query(WherePhrase.REGEX.name, enum=[p.name for p in WherePhrase])
+    q: str = Query("")
+    type: str = Query(WherePhrase.CONTAINS.name, enum=[p.name for p in WherePhrase])
     paging: Paging = Field(default_factory=Paging)
     order: OrderBy | None = None
 
 
+def get_search_param(  # noqa: PLR0917
+    q: str = Query(""),
+    type: str = Query(WherePhrase.CONTAINS.name, enum=[p.name for p in WherePhrase]),  # noqa: A002
+    page: int = Query(default=1, gt=0),
+    size: int = Query(default=100, gt=0),
+    n_detail: int = Query(default=1),
+    n_premise: int = Query(default=3),
+    n_conclusion: int = Query(default=3),
+    n_refer: int = Query(default=3),
+    n_referred: int = Query(default=3),
+    dist_axiom: int = Query(default=1),
+    dist_leaf: int = Query(default=1),
+    desc: bool = Query(default=True),  # noqa: FBT001
+) -> SearchParam:
+    """orderByがうまく変換されなかったので噛ませた."""
+    paging = Paging(page=page, size=size)
+    order = OrderBy(
+        n_detail=n_detail,
+        n_premise=n_premise,
+        n_conclusion=n_conclusion,
+        n_refer=n_refer,
+        n_referred=n_referred,
+        dist_axiom=dist_axiom,
+        dist_leaf=dist_leaf,
+        desc=desc,
+    )
+    return SearchParam(q=q, type=type, paging=paging, order=order)
+
+
 @knowde_router().get("/")
 def search_by_text(
-    param: SearchParam = Query(default=SearchParam()),
+    # param: SearchParam = Query(default=SearchParam()),
+    param: SearchParam = Depends(get_search_param),
 ) -> list[KAdjacency]:
     """文字列検索."""
     t = WherePhrase[param.type]
