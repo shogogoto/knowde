145177af8c69ee95d2d449a8d629b16b5aeeaed9
Shogo Goto <gotoadmn0605@gmail.com>
2025-06-26 09:26:34 +0900

    remove unused label repo

--- knowde/shared/__init__.py
+++ knowde/shared/__init__.py
@@ -6,9 +6,7 @@ import os
 from neomodel import config
 
 from .cli import *  # noqa: F403
-from .domain import Entity  # noqa: F401
 from .errors import *  # noqa: F403
-from .label_repo import *  # noqa: F403
 from .timeutil import TZ, jst_now, to_date  # noqa: F401
 from .types import NeoModel, NXGraph  # noqa: F401
 from .typeutil import *  # noqa: F403
--- knowde/shared/label_repo/__init__.py
+++
@@ -1,6 +0,0 @@
-"""共通repository."""
-
-from .base import LBase, RelBase  # noqa: F401
-from .query import query_cypher  # noqa: F401
-from .rel import RelUtil  # noqa: F401
-from .util import LabelUtil  # noqa: F401
