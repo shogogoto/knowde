2ff71824da37f145d95923db2b13d1a6e74b4639
Shogo Goto <gotoadmn0605@gmail.com>
2025-07-09 21:30:25 +0900

    fix num 関係を辿れなかったからlocationが見つからなかった

--- knowde/feature/knowde/detail/test_repo.py
+++ knowde/feature/knowde/detail/test_repo.py
@@ -2,8 +2,10 @@
 
 from uuid import UUID
 
+from fastapi.testclient import TestClient
 from pytest_unordered import unordered
 
+from knowde.api import root_router
 from knowde.conftest import async_fixture, mark_async_test
 from knowde.feature.knowde.detail import detail_knowde
 from knowde.feature.knowde.repo import save_text
@@ -175,5 +177,73 @@ async def test_detail_no_header(u: LUser):
     assert d.location.user.uid.hex == u.uid
 
 
-# def test_not_found_should_not_raise_error():
-#     detail_knowde(UUID("00000000-0000-0000-0000-000000000000"))
+@mark_async_test()
+async def test_not_found_should_not_raise_error(u: LUser):
+    """Regression test."""
+    s = """
+    # 神は数学者か?
+      @author マオリ・リヴィオ
+      @published 2017
+
+    ## 1. 謎
+
+      数学の偏在性と全能性:
+
+      「宇宙はまるで純粋数学者が設計したかのようだ」
+        by. ジェームズ・ジーンズ
+          イギリスの宇宙物理学者 1877-1946
+
+      P1 |「数学は、経験とは無関係な思考の産物なのに、\
+        なぜ物理的実在の対称物にこれほどうまく適合するのか」
+
+      ロジャー・ペンローズ=ペンローズ: オックスフォード大学の著名な数理物理学者
+
+      ペンローズの３世界:
+        1. 精神世界: 人間の表象の世界
+        2. 物質世界: 物理的実在の世界
+        3. 数学世界: プラトン主義の数学的形式の世界、イデア界
+          ex. ユークリッド幾何学、ニュートンの運動法則、ひも理論などの数学的モデル
+
+      ペンローズの３つの謎:
+        1. {物質世界}が{数学世界}の法則に従う
+          ex. `P1`
+          ! EF = EFfective
+          ex. EF |数学の不条理な有効性:
+            物理学の法則を定式化するのに数学という言語が似つかわしいというこの奇跡
+            {物質世界}が{数学世界}に従うという謎
+            by. ユージン・ウィグナー, ウィグナー: ノーベル賞を受賞した物理学者
+              when. 1902 ~ 1995
+        2. 心が{物質世界}から生まれる
+        3. {ペンローズの３世界}が不思議なほど結びついている
+
+      積極的な側面: 数学を応用して自然法則を構築するケース
+        {EF}を信じて{物質世界}から数学的法則を導き出すこと
+        ex. マクスウェル方程式、一般相対性理論
+        <-> 受動的な側面: 数学が意図せずに{物質世界}に応用されること
+          ハーディ: 純粋数学が{物質世界}に応用されるとは信じていなかった
+            イギリスの変わり者の数学者
+            when. 1877 ~ 1947
+            「数論を戦争に役立たせる道は誰も見出していない」
+              <-> 暗号に応用され
+          ! nameの区切り文字を","に変えるべきか
+          ハーディ=ワインベルクの法則: 進化の基本原理を説明する数学的モデル
+            以下を満たす大規模の遺伝子恒星は世代間で変化しない
+              無作為に交配する
+              個体の流出なし
+              突然変異なし
+              自然選択が発生しない
+          楕円の研究
+            by. メナイクモス
+              when. BC50
+            太陽系の惑星の軌道の記述へ応用された
+              by. ケプラー
+              by. ニュートン
+    """
+    _sn, _r = await save_text(u.uid, s)
+    tgt = LSentence.nodes.first(val="物理的実在の世界")
+    client = TestClient(root_router())
+    res = client.get(f"/knowde/sentence/{tgt.uid}")
+    assert res.is_success
+    uid = UUID(tgt.uid)
+    res = client.get(f"/knowde/sentence/{uid}")
+    assert res.is_success
