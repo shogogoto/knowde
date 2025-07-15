cbfcf2c11462e193790e807635afc9f20ed332c0
Shogo Goto <gotoadmn0605@gmail.com>
2025-07-15 18:37:10 +0900

    log user manager

--- knowde/feature/user/manager.py
+++ knowde/feature/user/manager.py
@@ -16,43 +16,30 @@ from .repo.db import AccountDB
 s = Settings()
 
 
+logger = logging.getLogger(__name__)
+
+
 class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
     """from fastapi-users."""
 
     reset_password_token_secret = s.KN_AUTH_SECRET
     verification_token_secret = s.KN_AUTH_SECRET
 
-    # @override
-    # async def on_after_login(
-    #     self,
-    #     user: User,
-    #     request: Request | None = None,
-    #     response: Response | None = None,
-    # ) -> None:
-    #     """SSOのブラウザからのレスポンスを取得."""
-    #     if response is not None and s.FRONTEND_URL is not None:
-    #         await super().get_login_response(user, response, self)
-    #         response.status_code = 303
-    #         response.headers["Location"] = s.FRONTEND_URL
+    @override
+    async def on_after_login(self, user, request=None, response=None):
+        logger.info("User has logged in. %s", user.id)
 
     @override
-    async def on_after_register(self, user, request=None):
-        logger = logging.getLogger(__name__)
-        logger.info(f"User {user.id}[{user.email}] has registered.")  # noqa: G004
+    async def on_after_register(self, user: User, request=None) -> None:
+        logger.info("User has registered. %s", user.id)
 
     @override
     async def on_after_forgot_password(self, user, token, request=None):
-        print(  # noqa: T201
-            f"User {user.id}[{user.email}] has forgot their password. "
-            f"Reset token: {token}",
-        )
+        logger.info("User has forgot password. %s", user.id)
 
     @override
     async def on_after_request_verify(self, user, token, request=None):
-        print(  # noqa: T201
-            f"Verification requested for user {user.id}[{user.email}]."
-            f"Verification token: {token}",
-        )
+        logger.info("User has requested email verification. %s", user.id)
 
 
 def get_user_manager() -> UserManager:
