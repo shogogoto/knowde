7bafe708dc4a5506f17d3c8f0adcab0ce17f0085
Shogo Goto <gotoadmn0605@gmail.com>
2025-07-16 20:15:20 +0900

    test seach user

--- knowde/feature/user/public/test_public.py
+++ knowde/feature/user/public/test_public.py
@@ -57,25 +57,30 @@ async def test_search_uuid_hyphenless_or_not(client: TestClient):
     assert len(res.json()) == 1
 
 
-# @mark_async_test()
-# async def test_search_username_or_id(client: TestClient):
-#     """ユーザーをusername または id で検索できる."""
-#     await LUser(uid="a0", email="u1@example.com", username="aaa").save()
-#     await LUser(uid="a2", email="u2@example.com", username="bbb").save()
-#     await LUser(uid="a3", email="u3@example.com", username="ccc").save()
-#     await LUser(uid="a4", email="u4@example.com", username="ddd").save()
-#     await LUser(uid="a9", email="u5@example.com", username="eee").save()
-#
-#     print(await LUser.nodes.all())
-#     res = client.get("/user/search/", params={"id": "a"})
-#     assert len(res.json()) == 1
-#
-#     res = client.get("/user/search/", params={"id": "aa"})
-#     assert len(res.json()) == 1
+@mark_async_test()
+async def test_search_username_or_id(client: TestClient):
+    """ユーザーをusername または id で検索できる."""
+    await LUser(uid="a0", email="u1@example.com", username="aaa").save()
+    await LUser(uid="a2", email="u2@example.com", username="bbb").save()
+    await LUser(uid="a3", email="u3@example.com", username="ccc").save()
+    await LUser(uid="a4", email="u4@example.com", username="ddd").save()
+    await LUser(
+        uid="aa9",
+        email="u5@example.com",
+        username="eeea",
+        display_name="hoge",
+    ).save()
+
+    res = client.get("/user/search/", params={"id": "aa"})
+    assert len(res.json()) == 2  # noqa: PLR2004
+    res = client.get("/user/search/", params={"id": "aaaa"})
+    assert len(res.json()) == 0
+    res = client.get("/user/search/", params={"display_name": "ho"})
+    assert len(res.json()) == 1
 
 
 @mark_async_test()
-async def test_search_username_or_id(client: TestClient):
+async def test_get_user_profile(client: TestClient):
     """ユーザーをusername または id で検索できる."""
     await LUser(uid="a0", email="u1@example.com", username="aaa").save()
     res = client.get("/user/profile/a")
