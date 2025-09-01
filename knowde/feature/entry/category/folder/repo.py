bc2f0bd2cca3f3360c0201914dcc66cbe3e9cc64
Shogo Goto <gotoadmn0605@gmail.com>
2025-09-01 22:52:47 +0900

    folder repo を namespace repo で置き換え

--- knowde/feature/entry/category/folder/repo.py
+++ knowde/feature/entry/category/folder/repo.py
@@ -6,13 +6,15 @@ from datetime import date
 from pathlib import PurePath
 from typing import TYPE_CHECKING
 
-from neomodel import INCOMING, AsyncTraversal
-from neomodel.async_.core import AsyncDatabase
-
-from knowde.feature.entry.errors import EntryAlreadyExistsError, EntryNotFoundError
-from knowde.feature.entry.label import LEntry, LFolder, LResource
-from knowde.feature.entry.namespace import fetch_namespace
-from knowde.shared.types import UUIDy, to_uuid
+from knowde.feature.entry import ResourceMeta
+from knowde.feature.entry.errors import EntryAlreadyExistsError
+from knowde.feature.entry.label import LFolder, LResource
+from knowde.feature.entry.namespace import (
+    fetch_namespace,
+    fill_parents,
+    save_resource,
+)
+from knowde.shared.types import UUIDy
 from knowde.shared.user.label import LUser
 
 if TYPE_CHECKING:
@@ -21,14 +23,8 @@ if TYPE_CHECKING:
 
 async def create_folder(user_id: UUIDy, *names: str) -> LFolder:
     """一般化フォルダ作成."""
-    match len(names):
-        case 0:
-            msg = "フォルダ名を1つ以上指定して"
-            raise ValueError(msg)
-        case 1:
-            return await create_root_folder(user_id, names[0])
-        case _:
-            return await create_sub_folder(user_id, *names)
+    ns = await fetch_namespace(user_id)
+    return await fill_parents(ns, *names)
 
 
 async def create_resource(
@@ -39,129 +35,23 @@ async def create_resource(
     urls: list[str] | None = None,
 ) -> LResource:
     """リソース作成."""
-    d = {"authors": authors, "published": published, "urls": urls}
-    match len(names):
-        case 0:
-            msg = "フォルダ名を1つ以上指定して"
-            raise ValueError(msg)
-        case 1:
-            return await create_root_resource(user_id, names[0], **d)
-        case _:
-            return create_sub_resource(user_id, *names, **d)
-
-
-async def check_already_root(lb: LUser, name: str) -> None:
-    """ユーザー直下でnameが既にあるか."""
-    trav = AsyncTraversal(lb, "OWNED", {"node_class": LEntry, "direction": INCOMING})
-    roots = await trav.all()
-    if name in [r.name for r in roots]:
-        raise EntryAlreadyExistsError
-
-
-async def create_root_folder(user_id: UUIDy, name: str) -> LFolder:
-    """直下フォルダ作成."""
-    u: LUser = await LUser.nodes.get(uid=to_uuid(user_id).hex)
-    await check_already_root(u, name)
-    f: LFolder = await LFolder(name=name).save()
-    await f.owner.connect(u)
-    return f
-
-
-async def create_root_resource(
-    user_id: UUIDy,
-    title: str,
-    authors: list[str] | None = None,
-    published: date | None = None,
-    urls: list[str] | None = None,
-) -> LResource:
-    """ユーザー直下のリソース."""
-    u: LUser = await LUser.nodes.get(uid=to_uuid(user_id).hex)
-    await check_already_root(u, title)
-    d = {
-        "title": title,
-        "authors": authors,
-        "published": published,
-        "urls": urls,
-    }
-    r = LResource(**d)
-    await r.save()
-    await r.owner.connect(u)
-    return r
-
-
-async def check_and_get_parent(user_id: UUIDy, root: str, *names: str) -> LFolder:
-    """配下に同名のentryがないことを確認して返す."""
     if len(names) == 0:
-        raise ValueError
-    create_name = names[-1]
-    parent, subs = await fetch_subfolders(user_id, root, *names[:-1])
-    if create_name in [s.name for s in subs if s is not None]:
-        p = "/".join([root, *names])
-        msg = f"/{p}'は既に存在しています"
-        raise EntryAlreadyExistsError(msg)
-    return parent
+        msg = "フォルダ名を1つ以上指定して"
+        raise ValueError(msg)
 
-
-async def create_sub_folder(user_id: UUIDy, root: str, *names: str) -> LFolder:
-    """サブフォルダ作成(同配下に名前の重複がないことを確認)."""
-    parent = await check_and_get_parent(user_id, root, *names)
-    sub = await LFolder(name=names[-1]).save()
-    await sub.parent.connect(parent)
-    return sub
-
-
-async def create_sub_resource(
-    user_id: UUIDy,
-    root: str,
-    *names: str,
-    authors: list[str] | None = None,
-    published: date | None = None,
-    urls: list[str] | None = None,
-) -> LResource:
-    """フォルダ配下に作成."""
-    parent = await check_and_get_parent(user_id, root, *names)
-    d = {
-        "name": names[-1],
-        "authors": authors,
-        "published": published,
-        "urls": urls,
-    }
-    r = await LResource(**d).save()
-    await r.parent.connect(parent)
-    return r
-
-
-async def fetch_subfolders(
-    user_id: UUIDy,
-    root: str,
-    *names: str,
-) -> tuple[LFolder, tuple[LFolder]]:
-    """ネットワークを辿ってフォルダとその配下をピンポイントに取得."""
-    n = len(names)
-    uid = to_uuid(user_id)
-    qs = [
-        f"MATCH (:User {{uid: $uid}})<-[:OWNED]-(f0:Entry {{ name: '{root}' }})",
-        *[
-            f"<-[:PARENT]-(f{i + 1}:Folder {{ name: '{name}' }})"
-            for i, name in enumerate(names)
-        ],
-        f"OPTIONAL MATCH (f{n})<-[:PARENT]-(sub:Entry)",
-        f"RETURN f{n}, sub",
-    ]
-    q = "\n".join(qs)
-    res = await AsyncDatabase().cypher_query(
-        q,
-        params={"uid": uid.hex},
-        resolve_objects=True,
+    m = ResourceMeta(
+        title=names[-1],
+        authors=authors or [],
+        published=published,
+        urls=urls or [],
+        path=tuple(names[:-1]) if len(names) > 1 else None,
     )
-    res = res[0]  # [0]  # 1要素の2重リスト[[...]]のはず
-    if len(res) == 0:
-        p = "/".join([root, *names])
-        msg = f"フォルダ'/{p}'が見つからない"
-        raise EntryNotFoundError(msg, res)
-
-    targets, subs = zip(*res, strict=False)
-    return targets[0], subs
+    ns = await fetch_namespace(user_id)
+    r = await save_resource(m, ns)
+    if r is None:
+        msg = f"{names[-1]} は既に存在します"
+        raise EntryAlreadyExistsError(msg)
+    return r
 
 
 async def move_folder(
