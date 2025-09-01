006aa92cce700ab5b37635e0c5991de7335ac735
Shogo Goto <gotoadmn0605@gmail.com>
2025-09-01 13:58:57 +0900

    file upload router 移動

--- knowde/api/__init__.py
+++ knowde/api/__init__.py
@@ -15,7 +15,6 @@ from knowde.api.middleware.logging import LoggingMiddleware
 from knowde.api.middleware.logging.log_config import setup_logging
 from knowde.api.middleware.transaction import Neo4jTransactionMiddleware
 from knowde.config.env import Settings
-from knowde.feature.entry.resource.repo.router import nxdb_router
 from knowde.feature.entry.router import entry_router
 from knowde.feature.knowde.router import knowde_router
 from knowde.feature.user.routers import auth_router, user_router
@@ -55,7 +54,6 @@ api.add_middleware(LoggingMiddleware)
 api.include_router(auth_router())
 api.include_router(user_router())
 api.include_router(entry_router())
-api.include_router(nxdb_router())
 api.include_router(knowde_router())
 
 
--- knowde/feature/entry/router.py
+++ knowde/feature/entry/router.py
@@ -5,11 +5,15 @@ from __future__ import annotations
 from pathlib import Path
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+import chardet  # 文字エンコーディング検出用
+from fastapi import APIRouter, Depends, UploadFile
 
-from knowde.feature.entry import NameSpace
+from knowde.feature.entry import NameSpace, ResourceMeta
 from knowde.feature.entry.category.folder.repo import fetch_namespace
+from knowde.feature.entry.label import LResource
 from knowde.feature.entry.namespace import ResourceMetas, sync_namespace
+from knowde.feature.entry.resource.repo.save import sn2db
+from knowde.feature.parsing.tree2net import parse2net
 from knowde.feature.user.domain import User
 from knowde.shared.user.router_util import auth_component
 
@@ -36,6 +40,33 @@ async def get_namaspace(
 ) -> NameSpace:
     """ユーザーの名前空間."""
     return await fetch_namespace(user.id)
+
+
+async def read_content(file: UploadFile) -> str:
+    """ファイルの内容を適切なエンコーディングで読み込む."""
+    content = await file.read()
+    encoding = chardet.detect(content)["encoding"] or "utf-8"
+    return content.decode(encoding)
+
+
+@router.post("/upload")
+async def read_file(
+    files: list[UploadFile],
+    user: Annotated[User, Depends(auth_component().current_user(active=True))],
+) -> None:
+    """ファイルからsysnetを読み取って永続化."""
+    ns = await fetch_namespace(user.id)
+    for f in files:
+        txt = await read_content(f)
+        sn = parse2net(txt)
+        meta = ResourceMeta.of(sn)
+        r = ns.get_resource_or_none(meta.title)
+        if r is None:
+            lb = await LResource(**meta.model_dump()).save()
+        else:
+            lb = LResource(**r.model_dump())
+            await lb.refresh()
+        sn2db(sn, lb.uid)
 
 
 @router.get("/resource/{resource_id}")
