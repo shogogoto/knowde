4f62613f9f28f96b811f24a1e0fdda5561d36bcd
Shogo Goto <gotoadmn0605@gmail.com>
2025-09-12 15:48:47 +0900

    move netowrk1_node0 -> resouce statistics

--- knowde/feature/entry/resource/stats/test_scorable.py
+++ knowde/feature/entry/resource/stats/test_scorable.py
@@ -1,8 +1,9 @@
 """test."""
 
 from knowde.feature.parsing.tree2net import parse2net
-from knowde.feature.stats.systats.nw1_n0.scorable import LRWTpl, SyScore
 from knowde.feature.stats.systats.nw1_n1.ctxdetail import Nw1N1Label
+
+from .scorable import LRWTpl, SyScore
 
 
 def test_scorable_to_edge() -> None:
