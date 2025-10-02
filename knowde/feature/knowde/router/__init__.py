4744d1de41e770e81f1134eed9c191970f107116
Shogo Goto <gotoadmn0605@gmail.com>
2025-10-02 10:14:43 +0900

    change knowde detail uid type UUID -> str

--- knowde/feature/knowde/router/__init__.py
+++ knowde/feature/knowde/router/__init__.py
@@ -1,7 +1,6 @@
 """router."""
 
 from functools import cache
-from uuid import UUID
 
 from fastapi import APIRouter, Depends
 
@@ -9,6 +8,7 @@ from knowde.feature.knowde import KnowdeDetail, KnowdeSearchResult
 from knowde.feature.knowde.repo import search_knowde
 from knowde.feature.knowde.repo.cypher import WherePhrase
 from knowde.feature.knowde.repo.detail import chains_knowde
+from knowde.shared.types import to_uuid
 from knowde.shared.user.router_util import TrackUser
 
 from .params import SearchParam, get_search_param
@@ -31,6 +31,6 @@ async def search_by_text(
 
 
 @knowde_router().get("/sentence/{sentence_id}")
-def detail(sentence_id: UUID, user: TrackUser = None) -> KnowdeDetail:
+def detail(sentence_id: str, user: TrackUser = None) -> KnowdeDetail:
     """knowde詳細."""
-    return chains_knowde(sentence_id)
+    return chains_knowde(to_uuid(sentence_id))
